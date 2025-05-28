fcf01101cd7266f0c29496d15e66fc996e3d35ca
binh.le <binh.le@trustifytechnology.com>
2025-05-28 18:36:30 +0700

    update

---
+++ src/main/java/com/trustify/rocketchat/model/RocketChatEndpoint.java
@@ -0,0 +1,32 @@
+package com.trustify.rocketchat.model;
+
+/**
+ * Enum representing Rocket.Chat API endpoints.
+ */
+public enum RocketChatEndpoint {
+
+    // Channel endpoints
+    CHANNELS_LIST("/channels.list"),
+    CHANNELS_CREATE("/channels.create"),
+    CHANNELS_DELETE("/channels.delete"),
+    CHANNELS_INFO("/channels.info"),
+    CHANNELS_MESSAGES("/channels.messages"),
+    CHAT_SEARCH("/chat.search"),
+    CHAT_POST_MESSAGE("/chat.postMessage"),
+    ROOMS_UPLOAD("/rooms.media");
+
+    private final String path;
+
+    RocketChatEndpoint(String path) {
+        this.path = path;
+    }
+
+    /**
+     * Gets the path for this endpoint.
+     *
+     * @return the path
+     */
+    public String getPath() {
+        return path;
+    }
+}
